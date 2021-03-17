c5e5c4e162d814e0ce513865ea46b857fef3ba22
Tom Dale <tom@imgix.com>
2021-03-16 22:16:47 -0700

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,44 @@
+require('dotenv').config()
+
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby API Rocket Example`,
+    description: `An example to show how to source data from API Rocket.`,
+    author: `tomdale`,
+    url: `https://apirockettogatsby.letsgoheadless.com`,
+    image: `https://images.unsplash.com/photo-1534796636912-3b95b3ab5986?blend64=aHR0cHM6Ly9kZW1vcy5pbWdpeC5uZXQvZGFyay1lbGxpcHNlLWdyYWRpZW50LmFpP2ZtPXBuZyZhcj0yOjE&blend-mode=multiply&blend-size=inherit&w=900&fit=crop&ar=3%3A1&txt64=RmFyIEZhciBBd2F5IE5ld3M&txt-color=white&txt-align=middle%2Ccenter&txt-fit=max&txt-size=72&auto=format,compress`,
+  },
+  /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-source-graphql`,
+      options: {
+        typeName: "apirocket",
+        fieldName: "apirocket",
+        url: "https://graphql.apirocket.io",
+        headers: {
+          Authorization: `Bearer ${process.env.APIROCKET_API_KEY}`,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: ["G-ETGGVHPR5J"],
+        gtagConfig: {
+          optimize_id: "OPT-NP53F4X",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        pluginConfig: {
+          head: false,
+          respectDNT: true,
+          exclude: ["/preview"]
+        }
+      }
+    },
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
+}
---
+++ src/components/ArticleList.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "gatsby";
+import parse from "html-react-parser";
+import Imgix from "react-imgix";
+
+const ArticleListItem = ({ article }) => {
+    return(
+		<Link to="/">
+			<p class="text-2xl p-2">{parse(article.title)}</p>
+            <Imgix
+        src={article.heroImage.url}
+        imgixParams={{ fit: "crop", crop: "focalpoint", ar: "1.8:1"}}
+        sizes="(min-width: 1024px) calc(33vw - 60px), (min-width: 768px) calc(50vw - 100px), calc(100vw - 70px)"
+        loading="lazy"
+        imgProps={{
+          alt: "FarFarAway News image"
+        }}
+        />
+			<p class="p-2">{parse(article.content)}</p>
+		</Link>
+    );
+};
+
+const ArticleList = ({ articles }) => (
+    <div class="flex flex-wrap">
+        {articles.map((article, i) => (
+            <div class="p-4 lg:w-1/3 md:w-1/2 sm:w-full" key={i}>
+                <ArticleListItem article={article} />
+            </div>
+        ))}
+    </div>
+);
+
+export default ArticleList;
